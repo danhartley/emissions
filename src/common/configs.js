c2453bdcc95020c8b1c33954089c7479af429fa1
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-22 18:40:25 +0100

    add hotcarbon config defaults

---
+++ src/common/configs.js
@@ -0,0 +1,17 @@
+export const hotcarbon = {
+  lighthouse: {
+    resolution: {
+      desktop: {
+        width: 1920,
+        height: 1080,
+        scrolled: 30000,
+      },
+      mobile: {
+        width: 1080,
+        height: 2400,
+        scrolled: 30000,
+      },
+    }
+  },
+  link: 'https://hotcarbon.org/assets/2024/pdf/hotcarbon24-final120.pdf',
+}
