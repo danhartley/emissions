41bfb2ccf608e01eb1885132d4cdc2be9d33bb3b
Daniel Hartley <daniel.hartley@altruistiq.com>
2024-07-23 09:19:52 +0100

    move entry types to constants

--- src/common/constants.js
+++ src/common/constants.js
@@ -2,3 +2,18 @@
 
 export const DB = 'emissionsDB'
 export const STORE = 'emissions'
+export const entryTypes = [
+    "element",
+    "event",
+    "first-input",
+    "largest-contentful-paint",
+    "layout-shift",
+    "long-animation-frame",
+    "longtask",
+    "mark",
+    "measure",
+    "navigation",
+    "paint",
+    "resource",
+    "visibility-state" 
+  ]
